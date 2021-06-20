f19233461f8763a2fcdba369cd90794816a15b98
ArtyomKorn <temka.kornilov@mail.ru>
2021-06-20 13:31:59 +0300

    20_06_2021_Calculator

---
+++ Calculator/CalculatorLib/ExponentiationOperation .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+    public class ExponentiationOperation : IOperation
+    {
+        public string OperatorCode => "^";
+        public int Apply(int operand1, int operand2)
+        {
+            return (int) Math.Pow(operand1, operand2);
+        }
+    }
+}
